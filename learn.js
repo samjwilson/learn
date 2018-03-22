5c8b57de26e1de3cc26f78abd7d39a5b6ab5b43d
samjwilson <samueljwilson@yahoo.com>
2018-03-21 21:47:03 -0600

    test user sign in

--- learn.js
+++ learn.js
@@ -1,5 +1,30 @@
- function runscript() 
- {
-     alert( 'Hello, world!' );
- }
+var database = [
+    {
+        username: "sam",
+        password: "supersecret"
+    }
+]
 
+var newsFeed = [
+    {
+        username: "Bobby",
+        timeline: "Pumped for Javascript!",  
+    },
+    {
+        username: "Sally",
+        timeline: "Javascript rocks!",
+    }
+]
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
+
+function signIn(user, pass) {
+    if (user === database[0].username && pass === database[0].password) {
+        console.log(newsFeed);
+    } else {
+        alert("Sorry, wrong username and password.")
+    }
+}
+
+signIn(userNamePrompt,passwordPrompt);
