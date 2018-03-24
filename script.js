23e7aabf42e94fcc55503025c9346819e4931ddd
samjwilson <samueljwilson@yahoo.com>
2018-03-23 21:54:25 -0600

    Learning the functions

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const obj = {
-    player: 'bobby',
-    experience: 100,
-    wizardLevel: false,
+const first = () => {
+    const greet = 'Hi';
+    const second = () => {
+        alert(greet);
+    }
+    return second;
 }
 
-const { player, experience } = obj;
-let { wizardLevel} = obj;
+const newFunc = first();
+newFunc();
